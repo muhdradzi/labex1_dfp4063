78111a30427c3850b09f9122515d7235aaf803a4
muhdradzi <aizawatakeru99@gmail.com>
2021-09-02 12:08:26 +0800

    try edit index

---
+++ index.php
@@ -0,0 +1,12 @@
+<h1>Cinderella</h1>
+
+<h3>Cinderella, the beautiful and kind-hearted daughter, sees her world turn </h3>
+<h3>upside down when her beloved mother dies, and her pained father remarries </h3>
+<h3>another woman, the wicked Lady Tremaine, who has two equally cruel daughters, </h3>
+<h3>the jealous Anastasia and Drizella. But, once more, things will go from bad to </h3>
+<h3>worse, When Cinderella's father, too, dies, leaving her all alone in the Lady's </h3>
+<h3>clutches to serve as her maid-of-all-work. Under those circumstances, a shabby </h3>
+<h3>and neglected Cinderella doesn't stand much of a chance of attending the King's </h3>
+<h3>royal ball--let alone, captivate the handsome Prince--unless she turns to her </h3>
+<h3>loving Fairy Godmother who has quite a few tricks up her sleeve. Nevertheless, </h3>
+<h3>will the wronged damsel ever find peace--and with it--her own Prince Charming ?</h3>
