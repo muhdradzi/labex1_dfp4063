33c970f98a4c68f3e7d60100e7aa84328d11b0f3
muhdradzi <aizawatakeru99@gmail.com>
2021-09-03 17:12:17 +0800

    try edit index

--- chapter.php
+++ chapter.php
@@ -6,14 +6,10 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-      <nav>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="synopsis.php">Synopsis</a></li>
-          <li><a href="about.php">About</a></li>
-          <li><a href="chapter.php">Chapter List</a></li>       
-        </ul>
-      </nav>
+
+  <div class="navbar">
+        <?php include "containingMenu.php"; ?>
+      </div>
 
       <h2>Chapter 1 : Into The Forest</h2>
       <br>
--- index.php
+++ index.php
@@ -6,14 +6,9 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-      <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="synopsis.php">Synopsis</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="chapter.php">Chapter List</a></li>
-        </ul>
-      </nav>
+      <div class="navbar">
+        <?php include "containingMenu.php"; ?>
+      </div>
         
       <?php
             $nama = 'Beauty and the Beast';
